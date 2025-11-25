088333b6ba9e381e74b49e043252142f453083d7
jui1960 <juifull196044@gmail.com>
2025-11-25 22:06:50 +0600

    codeforces problem seet

---
+++ 1.Data type/B_Basic_Data_Types.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    long long int b;
+    char c;
+    float f;
+    double dob;
+
+    cin >> a >> b >> c >> f >> dob;
+
+    cout << a << endl
+         << b << endl
+         << c << endl
+         << f << endl
+         << dob;
+
+    return 0;
+}
