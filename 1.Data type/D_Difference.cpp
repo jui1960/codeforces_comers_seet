c4bce09e56a23bc1a3366ea4b044631c78deaf48
jui1960 <juifull196044@gmail.com>
2025-11-25 22:18:00 +0600

    add

---
+++ 1.Data type/D_Difference.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    long long int ans = (a * b) - (c * d);
+    cout << "Difference = " << ans;
+    return 0;
+}
