5749a6eec5dc0f697dd0753a008309eed0ae2bca
jui1960 <juifull196044@gmail.com>
2025-11-27 21:50:56 +0600

    multiplecation

---
+++ 2.Loop/F_Multiplication_table.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    int mul = 0;
+    for (int i = 1; i <=12; i++)
+    {
+
+        cout<<n <<" * "<<i<<" = "<<n*i<<'\n';
+    }
+    
+    return 0;
+}
