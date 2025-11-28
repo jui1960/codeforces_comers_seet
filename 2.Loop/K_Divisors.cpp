d1cb1e9f68918671603d8dc5335d7262c97319e4
jui1960 <juifull196044@gmail.com>
2025-11-28 19:43:04 +0600

    divisor

---
+++ 2.Loop/K_Divisors.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    int div = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if(n%i == 0){
+            cout<<i<<'\n';
+        }
+        // cout<<i<<'\n';
+    }
+
+    return 0;
+}
