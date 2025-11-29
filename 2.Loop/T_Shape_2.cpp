fa40a48cc01dda12057ab330d495273f8d85b102
jui1960 <juifull196044@gmail.com>
2025-11-29 10:12:06 +0600

    shape2

---
+++ 2.Loop/T_Shape_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int ster = 1;
+    int space = n - 1;
+
+    for (int i = 0; i < n; i++) {
+
+        for (int k = 1; k <= space; k++) {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= ster; j++) {
+            cout << "*";
+        }
+
+        cout << "\n";
+
+        ster += 2;
+        space--;
+    }
+
+    return 0;
+}
