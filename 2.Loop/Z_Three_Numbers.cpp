da2bdd37dab2e7d328e8b5c84cddae193fda1332
jui1960 <juifull196044@gmail.com>
2025-12-12 22:15:02 +0600

    update

--- 2.Loop/Z_Three_Numbers.cpp
+++ 2.Loop/Z_Three_Numbers.cpp
@@ -1,11 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-    
+    int k, s;
+    cin >> k >> s;
+    long long ans = 0;
+    for (int x = 0; x <= k; x++)
+    {
+        for (int y = 0; y <= k; y++)
+        {
+            int z = s - x - y;
+            if (0 <= z && z <= k)
+                ans++;
+        }
+    }
+    cout << ans << '\n';
 
     return 0;
 }
