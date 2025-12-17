3ed8eccbf031fff4b228c900fc4c5f7fbe996bcd
jui1960 <juifull196044@gmail.com>
2025-12-17 22:09:07 +0600

    mirror array

--- 3.Array/V_Frequency_Array.cpp
+++ 3.Array/V_Frequency_Array.cpp
@@ -1,30 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n, m;
-    cin >> n >> m;
+    int n,m;
+    cin>>n>>m;
 
-    vector<int> v(n);
+    vector<int>v(n);
     for (int i = 0; i < n; i++)
     {
-        cin >> v[i];
+        cin>>v[i];
     }
 
-    map<int, int> mp;
-    for (int i = 0; i < n; i++)
+    map<int,int>mp;
+    for (int i = 0; i <n; i++)
     {
         mp[v[i]]++;
     }
 
-    for (int i = 1; i <= m; i++)
+    for (int i=1; i <=m; i++)
     {
-        cout << mp[i] << '\n';
+       cout<<mp[i]<<'\n';
     }
+    
+    
+    
+
 
     return 0;
 }
