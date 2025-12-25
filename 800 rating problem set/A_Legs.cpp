dfc57ff9996a3b970df0a0f91041d7db10b223bd
jui1960 <juifull196044@gmail.com>
2025-12-25 20:30:53 +0600

    a_legs

---
+++ 800 rating problem set/A_Legs.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if (n == 2)
+            cout << 1 << '\n';
+        else
+        {
+            int cnt = n / 4;
+            if (n % 4 != 0)
+            {
+                cnt++;
+                cout << cnt << '\n';
+            }
+            else
+                cout << cnt << '\n';
+        }
+    }
+
+    return 0;
+}
