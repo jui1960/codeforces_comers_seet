9a10475f141bc236fa5b4e20a1eb6150ca498d14
jui1960 <juifull196044@gmail.com>
2025-12-25 20:12:43 +0600

    optimize code

--- 800 rating problem set/A_Sequence_Game.cpp
+++ 800 rating problem set/A_Sequence_Game.cpp
@@ -1,3 +1,42 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int n, x;
+//         cin >> n;
+//         vector<int> v(n);
+//         for (int i = 0; i < n; i++)
+//         {
+//             cin >> v[i];
+//         }
+//         cin >> x;
+//         int flag = 0;
+//         sort(v.begin(), v.end());
+//         for (int i = 0; i < n; i++)
+//         {
+//             if (v[i] <= x && v[n - 1] >= x)
+//             {
+//                 flag = 1;
+//                 break;
+//             }
+//         }
+//         if (flag == 1)
+//             cout << "YES\n";
+//         else
+//             cout << "NO\n";
+//     }
+
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -18,18 +57,13 @@ int main()
             cin >> v[i];
         }
         cin >> x;
-        int flag = 0;
+
         sort(v.begin(), v.end());
-        for (int i = 0; i < n; i++)
+
+        if (v[0] <= x && v[n - 1] >= x)
         {
-            if (v[i] <= x && v[n - 1] >= x)
-            {
-                flag = 1;
-                break;
-            }
-        }
-        if (flag == 1)
             cout << "YES\n";
+        }
         else
             cout << "NO\n";
     }
