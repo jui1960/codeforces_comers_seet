e9bfea7316a60d0fb38d267e90dc49e9fb7fb21e
jui1960 <juifull196044@gmail.com>
2025-12-25 20:09:57 +0600

    sequence

---
+++ 800 rating problem set/A_Sequence_Game.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        cin >> x;
+        int flag = 0;
+        sort(v.begin(), v.end());
+        for (int i = 0; i < n; i++)
+        {
+            if (v[i] <= x && v[n - 1] >= x)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 1)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
