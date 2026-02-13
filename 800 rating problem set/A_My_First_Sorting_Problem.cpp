a692793cae0b1fbd7dfaf01cdcc5427113c64a26
jui1960 <juifull196044@gmail.com>
2026-02-13 21:44:15 +0600

    add

---
+++ 800 rating problem set/A_My_First_Sorting_Problem.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+     int x,y;
+     cin>>x>>y;
+     if(x>y){
+        swap(x,y);
+        cout<<x<<" "<<y<<'\n';
+     }
+     else{
+        cout<<x<<' '<<y<<'\n';
+     }
+    }
+    return 0;
+}
